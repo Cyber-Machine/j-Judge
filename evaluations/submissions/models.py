e76a02825c8b53f582dcfebffcaccffd64714c04
Maaz Karim <maazkarim02@gmail.com>
2022-02-17 00:39:29 +0530

    Made Submissions

---
+++ evaluations/submissions/models.py
@@ -0,0 +1,11 @@
+from tkinter import CASCADE
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Submission(models.Model):
+    user=models.OneToOneField(User,on_delete=CASCADE)
+    url=models.URLField(blank=True)
+    marks=models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
