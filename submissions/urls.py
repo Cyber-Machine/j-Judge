361dcce8a4d839f1cc8a8fdc6c8fc2818f847065
Maaz Karim <maazkarim02@gmail.com>
2022-03-01 23:51:42 +0530

    Added User registration

--- submissions/urls.py
+++ submissions/urls.py
@@ -4,5 +4,6 @@ from django.contrib.auth import views as auth_views
 from submissions import views
 
 urlpatterns = [path('', auth_views.LoginView.as_view(), name='Login'),path('logout', auth_views.LogoutView.as_view(), name='logout'),
-#path('register'),
-path('dashboard', views.dashboard,name='dashboard')]
+path('register', views.register, name="register"),
+path('dashboard', views.dashboard,name='dashboard')
+]
--- submissions/views.py
+++ submissions/views.py
@@ -1,15 +1,19 @@
 import re
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
-# def register(request):
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():                     #Save from SQL injection attack 
-#             form.save
-#     form = UserCreationForm()
-
-
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():                     #Save from SQL injection attack 
+            form.save()
+            redirect('login')
+    form = UserCreationForm()
+    args = {
+        'form': form,
+    }
+    return render(request, 'submissions/register.html',args)
+  
 def dashboard(request):
     return render(request, 'submissions/dashboard.html')
